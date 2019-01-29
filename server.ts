dfb836952f684a04f5f1e0c5b78aa6d19f43f404
Rudolfs Vikmanis <rudolfs.vikmanis@gmail.com>
2019-01-29 12:13:43 +0200

    Create websocket handler stub

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log("Hello!");
+import express from "express";
+import { Server } from "http";
+import SocketIO from "socket.io";
+
+const app = express();
+const http = new Server(app);
+const io = SocketIO(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
